ee6f9c67f3dec643be5eaeac7b9199b0d03f995e
Dizi <58800976+DiziASP@users.noreply.github.com>
2024-05-25 13:47:06 +0700

    feat: cleaning

--- app/index.tsx
+++ app/index.tsx
@@ -4,7 +4,7 @@ import { View, Text } from "react-native";
 export default function Screen() {
   return (
     <View className="flex-1 justify-center items-center gap-5 p-6 bg-secondary/30">
-      <Text className="text-white text-2xl">Hello, world!</Text>
+      <Text className="text-white text-2xl">Hello, Pa Yusep!</Text>
     </View>
   );
 }
