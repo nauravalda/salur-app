46883651f8e5b18a96fee8c233b2f6c7f8034305
Dizi <58800976+DiziASP@users.noreply.github.com>
2024-05-25 23:41:03 +0700

    feat: change splash screen

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,11 @@
 import * as React from "react";
-import { View, Text } from "react-native";
+import { View } from "react-native";
+import { Text } from "~/components/ui/text";
 
 export default function Screen() {
   return (
     <View className="flex-1 justify-center items-center gap-5 p-6 bg-secondary/30">
-      <Text className="text-white text-2xl">Hello, Pa Yusep!</Text>
+      <Text className="text-center text-2xl font-bold">Hello, world!</Text>
     </View>
   );
 }
